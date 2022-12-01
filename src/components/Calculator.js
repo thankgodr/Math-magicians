35c299abdb40478a9c2f58bbbcbc9e8a96f00be8
ThankGod Richard <thankgod@lemonade.finance>
2022-12-01 11:02:03 +0100

    refactored

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,70 +1,56 @@
-import React from 'react';
+import { useState } from 'react';
 import CalculatorButton from './CalculatorButton';
 import ResultView from './ResultView';
 import CalculatorFunction from './CalucalatorFunction';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      obj: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-    this.addMe = this.addMe.bind(this);
-    this.resultFormater = this.resultFormater.bind(this);
-  }
+const Calculator = () => {
+  const [obj, setObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  addMe(num) {
-    const res = calculate(this.state.obj, num);
-    this.setState(
-      {
-        obj: res,
-      },
-    );
-  }
+  const addMe = (num) => {
+    const res = calculate(obj, num);
+    setObj(res);
+  };
 
-  static padZero(num) {
-    return num == null ? 0 : num;
-  }
+  const padZero = (num) => (num == null ? 0 : num);
 
-  resultFormater() {
-    if (this.state.obj.total == null) {
-      return Calculator.padZero(this.state.obj.next);
+  const resultFormater = () => {
+    if (obj.total == null) {
+      return padZero(obj.next);
     }
-    if (this.state.obj.operation == null) {
-      return this.state.obj.total;
+    if (obj.operation == null) {
+      return obj.total;
     }
-    return Calculator.padZero(this.state.obj.next);
-  }
+    return padZero(obj.next);
+  };
 
-  render() {
-    return (
-            <div className='row calculator'>
-               <ResultView result={this.resultFormater()} />
-               <CalculatorFunction name={'AC'} className="col-md-3 btn_number" add={this.addMe} id={'ac'}/>
-               <CalculatorFunction name={'+/-'} className="col-md-3 btn_number" add={this.addMe} id={'plusminus'} />
-               <CalculatorFunction name={'%'} className="col-md-3 btn_number" add={this.addMe} id={'percent'}/>
-               <CalculatorFunction name={'÷'} className="col-md-3 function" add={this.addMe} id={'divide'}/>
-               <CalculatorButton name="7" add={this.addMe} />
-               <CalculatorButton name="8" add={this.addMe}/>
-               <CalculatorButton name="9" add={this.addMe}/>
-               <CalculatorFunction name={'x'} add={this.addMe} id={'multiply'}/>
-               <CalculatorButton name="4" add={this.addMe}/>
-               <CalculatorButton name="5" add={this.addMe}/>
-               <CalculatorButton name="6" add={this.addMe}/>
-               <CalculatorFunction name={'-'} add={this.addMe} id={'minus'}/>
-               <CalculatorButton name="1" add={this.addMe}/>
-               <CalculatorButton name="2" add={this.addMe}/>
-               <CalculatorButton name="3" add={this.addMe}/>
-               <CalculatorFunction name={'+'} add={this.addMe} id={'plus'}/>
-               <CalculatorButton name={'0'} className="col-md-6 btn_number" add={this.addMe}/>
-               <CalculatorButton name="." className="col-md-3 btn_number " add={this.addMe} />
-               <CalculatorFunction name={'='} className="col-md-3 function" add={this.addMe} id={'equal'}/>
-            </div>
-    );
-  }
-}
+  return (
+    <div className='row calculator'>
+      <ResultView result={resultFormater()} />
+      <CalculatorFunction name={'AC'} className="col-md-3 btn_number" add={addMe} id={'ac'}/>
+      <CalculatorFunction name={'+/-'} className="col-md-3 btn_number" add={addMe} id={'plusminus'} />
+      <CalculatorFunction name={'%'} className="col-md-3 btn_number" add={addMe} id={'percent'}/>
+      <CalculatorFunction name={'÷'} className="col-md-3 function" add={addMe} id={'divide'}/>
+      <CalculatorButton name="7" add={addMe} />
+      <CalculatorButton name="8" add={addMe}/>
+      <CalculatorButton name="9" add={addMe}/>
+      <CalculatorFunction name={'x'} add={addMe} id={'multiply'}/>
+      <CalculatorButton name="4" add={addMe}/>
+      <CalculatorButton name="5" add={addMe}/>
+      <CalculatorButton name="6" add={addMe}/>
+      <CalculatorFunction name={'-'} add={addMe} id={'minus'}/>
+      <CalculatorButton name="1" add={addMe}/>
+      <CalculatorButton name="2" add={addMe}/>
+      <CalculatorButton name="3" add={addMe}/>
+      <CalculatorFunction name={'+'} add={addMe} id={'plus'}/>
+      <CalculatorButton name={'0'} className="col-md-6 btn_number" add={addMe}/>
+      <CalculatorButton name="." className="col-md-3 btn_number " add={addMe} />
+      <CalculatorFunction name={'='} className="col-md-3 function" add={addMe} id={'equal'}/>
+    </div>
+  );
+};
+export default Calculator;
