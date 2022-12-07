d2dc8feb9ba8f1b732f357d2d14555f0e073d676
ThankGod Richard <thankgod@lemonade.finance>
2022-12-07 22:25:17 +0100

    linted

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,33 @@ const Home = () => (
         <h3>Welcome to our Page</h3>
 
         <p>
-            Justo urna potenti urna massa dignissimnullam sollicitudinduis diampellentesque dignissim ipsumpraesent.
-            Diampellentesque ridiculus suspendisse leo non dictumst pellentesque inceptos maecenas interdum suspendisse
-            molestie. Imperdiet sempellentesque porta euismod mollisfusce tinciduntsed vivamus iaculis diam duis fringilla
-            tortornulla maecenas muspraesent platea. Mus est efficitur arcucurabitur euismod leoetiam cubilia ac dui ex.
-            Class tinciduntsed turpisetiam volutpatvestibulum quis magnased vulputate fermentum utvestibulum arcucurabitur
-            ligulanunc. Neque eratnulla tinciduntsed torquent accumsanpellentesque tincidunt eu mauris arcu mattis aliquam
+            Justo urna potenti urna massa dignissimnullam sollicitudinduis diampellentesque
+            dignissim ipsumpraesent.
+            Diampellentesque ridiculus suspendisse leo non dictumst pellentesque inceptos maecenas
+            interdum suspendisse
+            molestie. Imperdiet sempellentesque porta euismod mollisfusce tinciduntsed vivamus
+            iaculis diam duis fringilla
+            tortornulla maecenas muspraesent platea. Mus est efficitur arcucurabitur euismod
+            leoetiam cubilia ac dui ex.
+            Class tinciduntsed turpisetiam volutpatvestibulum quis magnased vulputate
+            fermentum utvestibulum arcucurabitur
+            ligulanunc. Neque eratnulla tinciduntsed torquent accumsanpellentesque tincidunt
+            eu mauris arcu mattis aliquam
         </p>
-        <p>Justo urna potenti urna massa dignissimnullam sollicitudinduis diampellentesque dignissim ipsumpraesent.
-            Diampellentesque ridiculus suspendisse leo non dictumst pellentesque inceptos maecenas interdum suspendisse
-            molestie. Imperdiet sempellentesque porta euismod mollisfusce tinciduntsed vivamus iaculis diam duis fringilla
-            tortornulla maecenas muspraesent platea. Mus est efficitur arcucurabitur euismod leoetiam cubilia ac dui ex.
-            Class tinciduntsed turpisetiam volutpatvestibulum quis magnased vulputate fermentum utvestibulum arcucurabitur
-            ligulanunc. Neque eratnulla tinciduntsed torquent accumsanpellentesque tincidunt eu mauris arcu mattis aliquam</p>
+        <p>
+            Justo urna potenti urna massa dignissimnullam sollicitudinduis diampellentesque
+            dignissim ipsumpraesent.
+            Diampellentesque ridiculus suspendisse leo non dictumst pellentesque inceptos maecenas
+            interdum suspendisse
+            molestie. Imperdiet sempellentesque porta euismod mollisfusce tinciduntsed vivamus
+            iaculis diam duis fringilla
+            tortornulla maecenas muspraesent platea. Mus est efficitur arcucurabitur euismod
+            leoetiam cubilia ac dui ex.
+            Class tinciduntsed turpisetiam volutpatvestibulum quis magnased vulputate
+            fermentum utvestibulum arcucurabitur
+            ligulanunc. Neque eratnulla tinciduntsed torquent accumsanpellentesque tincidunt
+            eu mauris arcu mattis aliquam
+        </p>
     </div>
 );
 
